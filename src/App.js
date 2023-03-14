a37f1136aab83c33322aff3f3b3e58e79fabc966
Andrew Vreeland <andrewvreeland@gmail.com>
2023-03-14 02:01:06 -0400

    updated from class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// REBUILD AS A CLASS COMPONENT
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 1st IMPORTS 
+
+import React from 'react';
+import Header from './Header';
+
+// 2nd CREATE CLASS - will always have a render method 
+
+class App extends React.Component {
+  render (){
+
+    return (
+      <>
+    <Header />
+    <main />
+    </>
+    )
+  }
 }
 
+// EXPORT CLASS THE CLASS FOR OTHER FILES TO IMPORT
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from './App';
+
+class Header extends React.Component{
+  render(){
+    return <h1>Hello people!</h1>
+  }
+}
+
+
+
+export default Header;
